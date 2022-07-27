b7cacb7474da83b6ec7fd7b697b9f10e96f12ee9
jyt0106 <107200165+jyt0106@users.noreply.github.com>
2022-07-27 23:27:56 +0800

    Update 1.php

--- 1.php
+++ 1.php
@@ -2,9 +2,9 @@
 //初始化随机数生成器种子，这行代码也可以删除
 $seed = time();
 //获取随机数
-$num = rand(1,5);
+$num = rand(1,80);
 //拼接图片地址
-$picpath = "https://cdn.jsdelivr.net/gh/jyt0106/I/".$num.".png";
+$picpath = $picpath = "https://cdn.jsdelivr.net/gh/jyt0106/I/".$num.".png";
 //重定位到图片
 die(header("Location: $picpath"));
 ?>
